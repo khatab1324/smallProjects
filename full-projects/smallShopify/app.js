f17c987ee989ae2cb097118d4a444312ea29a014
kahtab1324 <k.s.fayyad.dev@gmail.com>
2024-05-25 13:37:02 +0300

    some updates

--- full-projects/smallShopify/app.js
+++ full-projects/smallShopify/app.js
@@ -13,7 +13,8 @@ const MongoStore = require("connect-mongo");
 //when you require connect-mongo for store session use the last version and read some doc pleas don't follow colt code that will cause errors for you there fix in leture number 586 is not huge
 const multer = require("multer");
 const methodOverride = require("method-override");
-const mongoSanitize = require("express-mongo-sanitize");
+// const mongoSanitize = require("express-mongo-sanitize"); //mongoSanitize is a module that sanitizes user-supplied data to prevent MongoDB Operator Injection.
+// const helmet = require("helmet"); //Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver bullet, but it can help!
 //====================for ejs======================
 const path = require("path");
 const ejsMate = require("ejs-mate");
@@ -28,12 +29,11 @@ const accountRouter = require("./routers/account");
 const productRouter = require("./routers/product");
 const reviewRouter = require("./routers/review");
 const payRouter = require("./routers/pay");
-
+const { log } = require("console");
 mongoose.set("strictQuery", false); //DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7. Use `mongoose.set('strictQuery', false);` if you want to prepare for this change. Or use `mongoose.set('strictQuery', true);` to suppress this warning.
 mongoose
   .connect("mongodb://127.0.0.1:27017/smallshopify", {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
     useUnifiedTopology: true,
   })
   .then(() => {
@@ -48,8 +48,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.engine("ejs", ejsMate);
 // =========================session====================
-const dbUrl = "mongodb://localhost:27017/myDatabase";
-
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/myDatabase";
 const storeSession = MongoStore.create({
   mongoUrl: dbUrl,
   touchAfter: 24 * 60 * 60,
@@ -60,9 +59,10 @@ const storeSession = MongoStore.create({
 storeSession.on("error", function (e) {
   console.log("session store error", e);
 });
+const secret = process.env.SECRET || "thisshouldbeabettersecret!";
 const sessionConfig = {
   name: "session",
-  secret: "thisSecretKey",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -73,7 +73,6 @@ const sessionConfig = {
   },
 };
 app.use(session(sessionConfig));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
@@ -93,8 +92,64 @@ app.use((req, res, next) => {
   res.locals.error = req.flash("error");
   next();
 });
-// =====================use router===============
+//=======================use helmet======================
+// app.use(mongoSanitize());
+// app.use(helmet({ contentSecurityPolicy: true }));
 
+// const scriptSrcUrls = [
+//   //here I allow jsut this secript to use if you use another ones it will not shown to you
+//   "https://stackpath.bootstrapcdn.com/",
+//   "https://kit.fontawesome.com",
+//   "https://kit.fontawesome.com/805e959fd5.js",
+//   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css",
+//   "https://cdnjs.cloudflare.com/",
+//   "//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css",
+//   "https://cdn.jsdelivr.net",
+//   // call fontawesome
+//   "https://fonts.googleapis.com/",
+// ];
+// const styleSrcUrls = [
+//   "https://kit-free.fontawesome.com/",
+//   "https://fonts.googleapis.com/",
+//   "https://use.fontawesome.com",
+//   "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css",
+//   "https://use.fontawesome.com/releases/v5.8.1/css/all.css",
+//   ,
+//   "https://www.dcnewsnow.com/wp-content/uploads/sites/14/2022/08/GettyImages-1133980246.jpg?strip=1",
+//   "//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css",
+//   "https://ka-f.fontawesome.com/releases/v6.5.1/css/free.min.css",
+//   "https://kit.fontawesome.com/805e959fd5.js",
+//   "https://cdn.jsdelivr.net", //By the time I got to implementing Bootstrap, Bootstrap was suggesting a single link tag for everything, JS and CSS. Because of this recommendation, the CSS styles from Bootstrap were also coming from "https://cdn.jsdelivr.net". This means I needed to add "https://cdn.jsdelivr.net" to the "styleSrcUrls" array, which Colt only includes in the "scriptSrcUrls" array. We can also remove "https://stackpath.bootstrapcdn.com/" from the style array.
+//   "https://stackpath.bootstrapcdn.com/",
+// ];
+// const fontSrcUrls = [
+//   "https://fonts.gstatic.com",
+//   "https://kit-free.fontawesome.com",
+// ];
+
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: ["self"],
+//       scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+//       styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+//       workerSrc: ["'self'", "blob:"],
+//       imgSrc: [
+//         "'self'",
+//         "blob:",
+//         "data:",
+//         "https://yt3.googleusercontent.com/vWyhCklj-SWS-hJOpYiVfHlHB49Lk-uIKLdtEhbt-FZ4PGQodc_tC7sr6Ww1o7tf_1hfIwfPng=s900-c-k-c0x00ffffff-no-rj",
+//         "https://cdn.techjuice.pk/wp-content/uploads/2019/07/online-shopping.jpg",
+//         ,
+//         "https://www.dcnewsnow.com/wp-content/uploads/sites/14/2022/08/GettyImages-1133980246.jpg?strip=1",
+//         `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`, //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! //and I use my name in .env to found the imges
+//         "https://images.unsplash.com/",
+//       ],
+//       fontSrc: ["'self'", ...fontSrcUrls],
+//     },
+//   })
+// );
+// =====================use router===============
 app.use(authenticateRouter);
 app.use(storeRouter);
 app.use(controleRouter);
@@ -107,6 +162,7 @@ app.get("/", (req, res) => {
 });
 
 app.use((err, req, res, next) => {
+  console.log("errrrrr", err);
   const { statusCode = 500 } = err;
   if (!err.message) err.message = "Oh No, Something Went Wrong!";
   res.status(statusCode).render("error", { err });
